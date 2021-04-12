99fa165e4eeb3b018ce57a1786148692b947cce4
Rajkumar1806 <82362272+Rajkumar1806@users.noreply.github.com>
2021-04-12 11:43:05 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginpageComponent } from './User/loginpage/loginpage.component';
+import { RegistrationFormComponent } from './User/registration-form/registration-form.component';
+import { ShowdataComponent } from './User/showdata/showdata.component';
+import { UploadComponent } from './user/upload/upload.component';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot([
+    { path:'app-loginpage', component:LoginpageComponent },
+    { path:'app-registration-form', component:RegistrationFormComponent },
+    { path: 'app-showdata', component:ShowdataComponent },
+    {path:'app-upload', component:UploadComponent}
+  ])],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
